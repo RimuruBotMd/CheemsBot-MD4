edbba2d2808432238dfe80e44414a2717cba183a
RimuruBotMd <108895247+RimuruBotMd@users.noreply.github.com>
2022-09-24 15:25:02 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -37,7 +37,7 @@ global.APIKeys = {
 
 //aumto functioner
 global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoreadpmngc = true //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = false //auto reading in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['6283866366437'] //ur owner number
+global.ownername = "Kanna Chan" //ur owner name
+global.ytname = "https://youtube.com/channel/UCOAloj1gW4OVGMtVDnNaq5w" //ur yt chanel name
+global.socialm = "https://github.com/XyzenzKannaV1" //ur github or insta name
+global.location = "Indonesia, Tegal, Jateng" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.owner = ['6283866366437']
+global.ownertag = '6283866366437' //ur tag number
+global.botname = 'SiestaBotzz' //ur bot name
+global.linkz = "https://youtube.com/channel/UCOAloj1gW4OVGMtVDnNaq5w" //your theme url which will be displayed on whatsapp
+global.websitex = "https://linktr.ee/kannachanv1" //ur website to be displayed
+global.botscript = 'https://github.com/XyzenzKannaV1' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "SiestaBotzz Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.author = "Kanna Chan" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
